2ddf9baa85668d87b2d6013306a1b1076d108217
Om Mahalle <133775655+OmMahalle@users.noreply.github.com>
2023-09-12 16:11:19 +0530

    Python Files

---
+++ Python files/test.py
@@ -0,0 +1,12 @@
+from PricePrediction import price
+from LocationPrediction import locn
+def test(price1,locan):
+    predictions1 = ""
+    price1 = price(predictions1)
+    print(price1)
+
+    key = ""
+    locan = locn(key)
+    print(locan)
+
+
